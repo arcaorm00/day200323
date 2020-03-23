e82a4e7a760c1e48d057e28edbb179020c44540d
arcaorm00 <62537764+arcaorm00@users.noreply.github.com>
2020-03-23 17:11:37 +0900

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,5 +3,7 @@ class hello
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World!");
+		System.out.println("This is a test code.");
+		System.out.println("This is a real life.");
 	}
 }
